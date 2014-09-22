49818a10f3868fbc45ff2383870798054e64a00a
Andy Brown <andyb@moo.com>
2014-09-22 21:19:49 +0100

    Add some seeds to the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create some Eliquids
+Eliquid.create(name: "Tabacco", volume: "10ml")
+Eliquid.create(name: "Menthol", volume: "10ml")
+Eliquid.create(name: "Blueberry", volume: "10ml")
+
+# Create some Subscription Plans
+SubscriptionPlan.create(name: "3X", eliquid_count: 3, interval_cost: 12, interval: "monthly", initial_cost: 0, initial_interval_count: 0, ecigarette_cost: 25)
+SubscriptionPlan.create(name: "3X UPFRONT", eliquid_count: 3, interval_cost: 12, interval: "monthly", initial_cost: 36, initial_interval_count: 3, ecigarette_cost: 0)
+SubscriptionPlan.create(name: "4X", eliquid_count: 4, interval_cost: 15, interval: "monthly", initial_cost: 0, initial_interval_count: 0, ecigarette_cost: 25)
+SubscriptionPlan.create(name: "4X UPFRONT", eliquid_count: 4, interval_cost: 15, interval: "monthly", initial_cost: 45, initial_interval_count: 3, ecigarette_cost: 0)
+SubscriptionPlan.create(name: "5X", eliquid_count: 5, interval_cost: 18, interval: "monthly", initial_cost: 0, initial_interval_count: 0, ecigarette_cost: 25)
+SubscriptionPlan.create(name: "5X UPFRONT", eliquid_count: 5, interval_cost: 18, interval: "monthly", initial_cost: 54, initial_interval_count: 3, ecigarette_cost: 0)
