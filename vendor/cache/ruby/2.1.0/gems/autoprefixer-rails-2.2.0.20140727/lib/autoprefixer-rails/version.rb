c4f4b4eb158e955acf75b57ae9142f16e19eda8d
Jak Spalding <jak@moo.com>
2014-10-23 10:16:21 +0100

    lets ignore vendor

--- vendor/cache/ruby/2.1.0/gems/autoprefixer-rails-2.2.0.20140727/lib/autoprefixer-rails/version.rb
+++
@@ -1,3 +0,0 @@
-module AutoprefixerRails
-  VERSION = '2.2.0.20140727'.freeze unless defined? AutoprefixerRails::VERSION
-end
--- vendor/cache/ruby/2.1.0/gems/bootstrap-sass-3.2.0.1/test/dummy_rails/app/assets/javascripts/application.js
+++
@@ -1,2 +0,0 @@
-//= require jquery.fullPage
-//= require bootstrap-sprockets
