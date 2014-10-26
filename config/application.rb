91771f50e8b8b0f93347b71a3f9a98d6e1c3ac9c
Andy Brown <andyb@moo.com>
2014-10-26 08:30:10 +0000

    hopefully bower will now build assets on heroku with the new build pack for heroku

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,17 @@ module Vaporize
 
 
         config.assets.enabled = true
+
+        # We don't want the default of everything that isn't js or css, because it pulls too many things in
+        config.assets.precompile.shift
+
+        # Explicitly register the extensions we are interested in compiling
+        config.assets.precompile.push(Proc.new do |path|
+            File.extname(path).in? [
+                '.html', '.slim',                 # Templates
+                '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+                '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+            ]
+        end)
     end
 end
